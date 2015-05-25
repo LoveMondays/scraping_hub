a52b17ce2d69dadf73e745b2f3abd8709ede062c
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2015-05-25 17:28:19 -0300

    Map the job service to list all jobs
    * search all jobs paginated

--- spec/scraping_hub/job_spec.rb
+++ spec/scraping_hub/job_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe ScrapingHub::Job do
+  describe '.search' do
+    context 'when the stub property is true' do
+      before { allow(ScrapingHub::Setup).to receive(:stub?) { true } }
+
+      it { expect(described_class.search).to eq [] }
+    end
+  end
 end
